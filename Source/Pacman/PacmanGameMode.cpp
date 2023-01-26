f7d7a4426efc8abf9c74d72a40a79f9a0138d416
Luis <impel.up@gmail.com>
2023-01-25 23:30:57 -0600

    [Fix] An error with the current url of the BP_ThirdPersonCharacter was fixed.

--- Source/Pacman/PacmanGameMode.cpp
+++ Source/Pacman/PacmanGameMode.cpp
@@ -7,7 +7,7 @@
 APacmanGameMode::APacmanGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Downloads/StartingAssets/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
